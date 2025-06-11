4d92cd8df9129e63701644547bf47489ef8e5a56
Dervanius <bristanovic@hotmail.com>
2025-06-11 22:26:18 +0200

    Complete Version

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-
 print("No more questions.")
+print(f"Your final score is {quiz.score}/{len(quiz.question_list)}.")
